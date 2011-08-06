ed25e1f71295f30c1bda79b692b875ff50c648e4
Justin John McCandless <mcjustin@umich.edu>
2011-08-06 17:43:04 -0500

    Version 7.0 - re-setup directory to fix all the extra files for good

---
+++ private/config.php
@@ -0,0 +1,16 @@
+<?php
+
+/******************************************************************************
+ *
+ * This file contains configuration parameters needed to access the db
+ * Note that it is NOT included in the git repo!
+ *
+******************************************************************************/
+
+
+$server = "localhost";
+$username = "?";
+$password = "?";
+$db = "justinmccandless";
+
+?>
