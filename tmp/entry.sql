35fc0141b3ce4afb932d30fe044834e1a52d524e
Justin John McCandless <mcjustin@umich.edu>
2011-08-06 16:48:45 -0500

    Version 7.0 - basic yet working version with a solid foundation for adding new features

---
+++ tmp/entry.sql
@@ -0,0 +1,14 @@
+INSERT INTO Entries VALUES (41, 42, 'http://www.justinmccandless.com/index.php?entry=42', 'Blog Fixed', '2011-07-25 18:00:00', '', 'Websites', 
+'
+<p>
+After far too long, I\'ve finally had enough time to sit down and work on bringing this blog back to life.  It\'s been a tough last year of university and I haven\'t had much time to work on this, but now that I don\'t have homework to eat up every second of my life I\'ve been able to work a bit on some more stuff for fun.  
+</p>
+<p>
+The blog is in the most basic form possible right now.  An incredibly simple framework and no support for making entries in any way other that writing SQL queries.  But, I\'ll be bringing things back to life as I go and adding some new features as well.
+</p>
+<p>
+In the meantime I\'m currently living and working in Lima, Peru, for the next few months, so I have plenty to write about now that the blog works again!  I\'ll try to catch up on the technical side and the content soon.
+</p>
+'
+);
+
