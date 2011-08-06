baf8730ec3779a6e358a137c65250a1b2c1ff01d
Justin John McCandless <mcjustin@umich.edu>
2011-08-06 16:53:30 -0500

    Version 7.0 - just deleted a bunch of old files from the repo

--- scripts/preload.js
+++
@@ -1,8 +0,0 @@
-   home= new Image(125,35);
-   home.src="http://www.justinmccandless.com/images/homeo.jpg";
-   archive= new Image(125,35);
-   archive.src="http://www.justinmccandless.com/images/archiveo.jpg";
-   aboutme= new Image(125,35);
-   aboutme.src="http://www.justinmccandless.com/images/aboutmeo.jpg";
-   links= new Image(125,35);
-   links.src="http://www.justinmccandless.com/images/linkso.jpg";
