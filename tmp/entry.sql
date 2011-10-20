0e09ab9b4f00365c522d7ac331f86ce3ac6cca5e
justin <justin@justin-laptop-ubuntu.(none)>
2011-10-19 20:14:30 -0500

    added pics for volunteering post

--- tmp/entry.sql
+++ tmp/entry.sql
@@ -1,44 +1,26 @@
-INSERT INTO Entries VALUES (55, 56, 'http://www.justinmccandless.com/index.php?entry=55', 'To Lima', '2011-10-10 18:00:00', '', 'Travel', 
+INSERT INTO Entries VALUES (56, 57, 'http://www.justinmccandless.com/index.php?entry=56', 'Transportation in Lima', '2011-10-19 20:00:00', '', 'Travel', 
 '
 <p>
-It has been my goal to work abroad after gradaution since I started thinking about what I would do after graduation.  I spent awhile looking for a good internship abroad during my senior year and after graduation, where I could develop my technical skills, learn about business in another country, and have the cultural and language immersion that you can only get abroad.  I would have been happy to go almost anywhere in the world, but month or so after graduation I found myself with this offer from Telefonica in Lima, not too far from where I did my first internship in Ecuador.  The job seemed to be the exact kind of thing that would get me to come back to Latin America.  I could work at a large company in a big city for a moderate amount of time and get the opposite side of the experience I had in Ecuador two years ago, while being immersed in a way I couldn\'t by starting fresh in a new part of the world.  I was a bit thrown off to go from a big job search to actually having an appealing offer, but I decided to cash in and take it.
+Telefonica has a big volunteering program here, and my boss invited me to participate in an event where we act as "big brothers/sisters" to some elementary school kids for an afternoon.  I always enjoy stuff like that, so I signed up and spent the day volunteering last Friday.
 </p>
-<img src = "http://www.justinmccandless.com/uploads/images/toLima0.jpg" alt = "Image Not Found">
+<img src = "http://www.justinmccandless.com/public/uploads/images/volunteering1.jpg" alt = "Image not found">
 <p>
-A month or so later I had moved all of my things from the past four years out of my apartment in Michigan back home to Vegas and was saying goodbye to my family and flying to Lima.  And here I\'ve been for the past 3 months.
+We decorated our cafeteria like crazy and welcomed all the kids there with some free food and a quick announcement when they got off their school bus.  I met my "little brother", an 11 year old name Ángel.  I took him on a tour of our building and showed him where I work and all the other offices.  He said his favorite subject is computer class, so it was cool to get to show him where our engineers work.  After we finished my building we ran into two of my coworkers giving tours as well, so we joined with them and went to talk to all the people they know in another building.  The kids had a warm welcome at each office and got a ton of Telefonica swag, I was actually kind of jealous...
 </p>
+<img src = "http://www.justinmccandless.com/public/uploads/images/volunteering2.jpg" alt = "Image not found">
+<img src = "http://www.justinmccandless.com/public/uploads/images/volunteering3.jpg" alt = "Image not found"> 
 <p>
-I\'m an intern in software development at Telefonica.  Most of my coworkers manage the development of internal software systems since actual coding is done by third-party consulting companies, but I get to be an actual developer on one project as well as manage a group of consultants in another.  I work from 9 to 4 on a good day (with a long lunch starting at 1).  The internship started at the very beginning of July, and my last day of work is in early December.  There are 8 other interns here working for Telefonica through AIESEC as well, from a bunch of different countries (Colombia, Mexico, Bulgaria, France, Spain).  We all work in different areas, most in different buildings, but we have a really cool group to hang out with after work.
+Afterwards we all met back in the cafeteria and then boarded the bus to go to the "Parque de las Aguas", a really nice park full of fountains in Lima.  They let us loose in the park and I got to run around with the kids and get a little bit wet.  We had a picnic lunch later in the grass, and the kids finally realized I was a foreigner.  After that point they never stopped asking me to translate every word they could think of (including their names) into "American".  The day felt really short when we had to say goodbye and the kids went back to school on their bus.  I had a ton of fun (and I hope the kids did too), even though somehow I managed to get sunburnt through the ever present clouds in Lima.
 </p>
+<img src = "http://www.justinmccandless.com/public/uploads/images/volunteering4.jpg" alt = "Image not found">
+<img src = "http://www.justinmccandless.com/public/uploads/images/volunteering5.jpg" alt = "Image not found"> 
 <p>
-I live with 2 Colombian interns in a 3 bedroom apartment in Surco, a more residential district of Lima.  The location is more out of the way, but it is one of the nicer places to live.  There are tons of parks within walking distance, and little stores and restaurants on every corner as always.  Two bigger supermarkets are a few blocks away, as well as a KFC and a McDonalds.  
+And finally, this will be in a Telefónica publication!  Sorry for the picture of text.
 </p>
-<p>
-I work in Surquillo, a less nice district, in the same building as one of my roommates.  It seems to be kind of in the shadow of one of Lima\'s nicer skyscraper filled districts.  There are plenty of restaurants around though, and it\'s not too far from things, so it\'s not too bad.  It takes me about 40 minutes to get to work every morning from my apartment using the crazy private bus system in Lima.
-</p>
-<p>
-I\'ve been very fortunate once again to be supported by an incredible AIESEC chapters, from the University of the Pacific here.  These guys are one of the most well-oiled LCs I\'ve seen, and they have gone well out of their way to help me multiple times.  One AIESECer took an afternoon off work on two different days to walk me through the most awful bureacratic process ever to receive a package my dad sent me.  They have made me feel much more at home in this huge city.
-</p>
-<p>
-Alright, I just wanted to give a quick update about what brought me out here and what I\'m doing.  I have a lot more written about Lima and some trips and other things going on, which I\'ll post soon.
-</p>
+<img src = "http://www.justinmccandless.com/public/uploads/images/volunteering6.jpg" alt = "Image not found"> 
 '
-);
 
 /*
-This job seemed to be the exact idea of something that would get me to come back to Latin America.  I could work at a large company in a big city for a moderate amount of time and get the opposite side of the experience I had in Ecuador two years ago.  I was a bit thrown off to go from a big job search to actually having an appealing offer, but I decided to cash in and take it.
-
-A month or so later I had moved all of my things from the past four years out of my apartment in Michigan back home to Vegas and was saying goodbye to my family and flying to Lima.  And here I've been for the past 3 months.
-
-I'm an intern in software development at Telefonica.  Most of my coworkers manage the development of internal software systems since actual coding is done by third-party consulting companies, but I get to be an actual developer on one project as well as manage a group of consultants in another.  I work from 9 to 4 on a good day (with a long lunch starting at 1).  The internship started at the very beginning of July, and my last day of work is in early December.  There are 8 other interns here working for Telefonica through AIESEC as well, from a bunch of different countries (Colombia, Mexico, Bulgaria, France, Spain).  We all work in different areas, most in different buildings, but we have a really cool group to hang out with after work.
-
-I live with 2 Colombian interns in a 3 bedroom apartment in Surco, a more residential district of Lima.  The location is more out of the way, but it is one of the nicer places to live.
-
-I work in Surquillo, a less nice district, in the same building as one of my roommates.  It seems to be kind of in the shadow of one of Lima's nicer skyscraper filled districts.  There are plenty of restaurants around though, and it's not too far from things, so it's not too bad.  It takes me about 40 minutes to get to work every morning from my apartment.
-
-
-
-
 --
 
 Last weekend I was able to travel again so I need to write about it!  This trip was with a bunch of our interns here to Ica in the desert south of Lima.  Nazca, where the Nazca lines are, is in this area.  I was really excited about these, but you pretty much have to take a plane to see them so I wasn't able to.  We did wind up seeing another similar petroglyph though nearby...
@@ -54,15 +36,6 @@ Volunteering
 
  
 
-Telefonica has a big volunteering program here, and my boss invited me to participate in an event where we act as "big brothers/sisters" to some elementary school kids for an afternoon.  I always enjoy stuff like that, so I signed up and spent the day volunteering last Friday.
-
- 
-
-We decorated our cafeteria like crazy and welcomed all the kids there with some free food and a quick announcement when they got off their school bus.  I met my "little brother", an 11 year old name Ángel.  I took him on a tour of our building and showed him where I work and all the other offices.  He said his favorite subject is computer class so it was cool to get to show him where our engineers work.  After we finished my building we ran into two of my coworkers giving tours as well, so we joined with them and went to talk to all the people they know in another building.  The kids had a warm welcome at each office and got a ton of Telefonica swag, I was actually kind of jealous...
-
- 
-
-Afterwards we all met back in the cafeteria and then boarded the bus to go to the "Parque de las Aguas", a really nice park full of fountains in Lima.  They let us loose in the park and I got to run around with the kids and get a little bit wet.  We had a picnic lunch later in the grass, and the kids finally realized I was a foreigner.  After that point they never stopped asking me to translate every word they could think of (including their names) into "American".  The day felt really short when we had to say goodbye the kids went back to school on their bus.  I had a ton of fun (and I hope the kids did too), even though somehow I managed to get sunburnt through the ever present clouds in Lima.
 
  
 
