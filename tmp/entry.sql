458cbdeeb50db10235ef7a758d9003f3235fcd73
justin <justin@justin-laptop-ubuntu.(none)>
2011-10-30 23:10:05 -0500

    added a pic for Volunteering cont'd, added that post in entries, tweaked favicon

--- tmp/entry.sql
+++ tmp/entry.sql
@@ -1,4 +1,23 @@
-INSERT INTO Entries VALUES (56, 57, 'http://www.justinmccandless.com/index.php?entry=56', 'Transportation in Lima', '2011-10-19 20:00:00', '', 'Travel', 
+INSERT INTO Entries VALUES (57, 58, 'http://www.justinmccandless.com/index.php?entry=58', 'Volunteering Cont\'d', '2011-10-30 22:00:00', '', 'Travel', 
+'
+<p>
+I just talked about the day I spent volunteering through work in the <a href = "http://www.justinmccandless.com/index.php?entry=57">previous post</a>, but I wanted to talk about some more cultural stuff from the day as well (since I am in another country).
+</p>
+<img src = "volunteeringCont1.jpg" alt = "Image not found">
+<p>
+One of the main things I wanted to mention was just the difference in how my workmates treated the underprivileged kids compared to how I feel like it would go in the US.  Firstly, they seemed to be just showering the kids in positive reinforcement.  From when they first arrived and we all cheered them in, and practically served them food, to later going through the offices everyone we ran into was loading them down with any pens, notebooks, food, etc. that they could find.  It seemed like a lot of positivity when Americans might be more conscious about "spoiling" them or giving them too much.  Also, many of my coworkers seemed to take less of a friend or authority role, and instead be more like a parent, and almost lecture the kids about the most basic stuff.  I heard the most typical things said to the kids like "Stay in school", "Don\'t play with matches", "Have faith in God even if your parents don\'t", etc., and me as an American felt like this would be seen as stepping on the parent\'s toes and doing their job in an overly forward way in the US.  Note that the religious comment obviously has a much different feel here in Peru where the vast majority of the people and the schools are Catholic (including this school), but it still seemed like they were preaching to these kids they just met like they won\'t get it anywhere else.
+</p>
+<p>
+This could have been affected by the fact that the kids were not like the neighbor\'s kids or something, they were from a very poor area outside of Lima.  I was told most parents worked in furniture factories and the like by my coworkers, but I\'m not personally familiar with the area.  Some of them had fairly beat up back packs or shoes, but it wasn\'t horribly obvious by hanging out with them.  My "little brother" in particular did have it bad though, he was missing about half of one of his fingers and his upper lip area was dented inward so that it was pretty hard to understand his speech.  I couldn\'t tell if it was a birth defect or it had been caused later in life.  He got by fine though, and he seemd to be accepted like normal by the other kids.
+</p>
+<p>
+These were just a few interesting things I noticed while volunteering and wanted to write about.  I had a ton of fun volunteering here, and I hope we gave the kids an awesome day.  For me, it was an incredible experience being able to see and participate in something like this, not just back in Las Vegas but in another country!
+</p>
+');
+
+
+
+/*
 '
 Speaking a new language and dealing with ignorance
 <p>
@@ -13,7 +32,6 @@ As negative as this post may have sounded, things like this actually just remind
 </p>
 '
 
-/*
 --
 
 Last weekend I was able to travel again so I need to write about it!  This trip was with a bunch of our interns here to Ica in the desert south of Lima.  Nazca, where the Nazca lines are, is in this area.  I was really excited about these, but you pretty much have to take a plane to see them so I wasn't able to.  We did wind up seeing another similar petroglyph though nearby...
@@ -27,41 +45,13 @@ We did go on a boat to see that cactus drawing, but the main attraction wasn't t
 /*
 Volunteering
 
- 
+--- 
 
 
  
 
  
 
-Volunteering, Serious Edition
-
- 
-
-I just talked about the day I spent volunteering through work in the previous post (link), but I wanted to talk about some more serious topics from that day as well. 
-
- 
-
-The main thing I wanted to mention was just the difference in how my workmates treated the underprivileged kids compared to how I feel like it would go in the US.  Firstly, they seemed to be just showering the kids in positive reinforcement.  From when they first arrived and we all cheered them in, and practically served them food, to later going through the offices everyone we ran into was loading them down with any pens, notebooks, food, etc. that they could find.  It seemed like a lot of positivity when Americans might be more conscious about "spoiling" them or giving them too much.  Also, many of my coworkers seemed to take less of a friend or authority role, and instead be more like a parent, and almost lecture the kids about the most basic stuff.  I heard the most typical things said to the kids like "Stay in school", "Don't play with matches", "Have faith in God even if your parents don't", etc., and me as an American felt like they were stepping on the parent's toes and doing their job in a kind of ridiculous way.  Note that the religious comment obviously has a much different feel here in Peru where the vast majority of the people and the schools are Catholic (including this school), but it still seemed like they were preaching to these kids they just met like they won't get it anywhere else.
-
- 
-
-This could have been affecting by the fact that the kids were not like the neighnor's kids or something, they were from a very poor area outside of Lima.  I was told most parents worked in furniture factories and the like by my coworkers, but I'm not personally familiar with the area.  Some of them had fairly beat up back packs or shoes, but it wasn't horribly obvious by hanging out with them.  My "little brother" in particular did have it bad though, he was missing about half of one of his fingers and his upper lip area was dented inward so that it was pretty hard to understand his speech.  I couldn't tell if it was a birth defect or it had been caused later in life.  He got by fine though, and he seemd to be accepted like normal by the other kids.
-
- 
-
-These were just a few interesting things I noticed while volunteering and wanted to write about.
-
-Este mensaje y sus adjuntos se dirigen exclusivamente a su destinatario, puede contener información privilegiada o confidencial y es para uso exclusivo de la persona o entidad de destino. Si no es usted. el destinatario indicado, queda notificado de que la lectura, utilización, divulgación y/o copia sin autorización puede estar prohibida en virtud de la legislación vigente. Si ha recibido este mensaje por error, le rogamos que nos lo comunique inmediatamente por esta misma vía y proceda a su destrucción.
-
-The information contained in this transmission is privileged and confidential information intended only for the use of the individual or entity named above. If the reader of this message is not the intended recipient, you are hereby notified that any dissemination, distribution or copying of this communication is strictly prohibited. If you have received this transmission in error, do not read it. Please immediately reply to the sender that you have received this communication in error and then delete it.
-
-Esta mensagem e seus anexos se dirigem exclusivamente ao seu destinatário, pode conter informação privilegiada ou confidencial e é para uso exclusivo da pessoa ou entidade de destino. Se não é vossa senhoria o destinatário indicado, fica notificado de que a leitura, utilização, divulgação e/ou cópia sem autorização pode estar proibida em virtude da legislação vigente. Se recebeu esta mensagem por erro, rogamos-lhe que nos o comunique imediatamente por esta mesma via e proceda a sua destruição
-
-
-
-
-----
 
 Lima
 
