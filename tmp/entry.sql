f211c061a4f24082250ec37cfc9dd82980555604
justin <justin@justin-laptop-ubuntu.(none)>
2011-11-01 14:00:50 -0500

    block paragraph alignment, and pics for Ica post

--- tmp/entry.sql
+++ tmp/entry.sql
@@ -1,20 +1,65 @@
-INSERT INTO Entries VALUES (57, 58, 'http://www.justinmccandless.com/index.php?entry=58', 'Volunteering Cont\'d', '2011-10-30 22:00:00', '', 'Travel', 
+INSERT INTO Entries VALUES (58, 59, 'http://www.justinmccandless.com/index.php?entry=59', 'Ica', '2011-11-01 13:00:00', '', 'Travel', 
 '
 <p>
-I just talked about the day I spent volunteering through work in the <a href = "http://www.justinmccandless.com/index.php?entry=57">previous post</a>, but I wanted to talk about some more cultural stuff from the day as well (since I am in another country).
+The other weekend I was able to travel again so I need to write about it!  This trip was with a bunch of our interns here to Ica in the desert south of Lima.  We stayed in a small town outside Ica called Huacachina, which is honestly like an oasis in the middle of sand dunes.  We got to see a vineyard, go out on the dunes, and take a boat to some islands full of birds called Las Islas Ballestas.
 </p>
-<img src = "http://www.justinmccandless.com/uploads/images/volunteeringContd1.jpg" alt = "Image not found">
+<img src = "http://www.justinmccandless.com/uploads/images/ica1.jpg">
 <p>
-One of the main things I wanted to mention was just the difference in how my workmates treated the underprivileged kids compared to how I feel like it would go in the US.  Firstly, they seemed to be just showering the kids in positive reinforcement.  From when they first arrived and we all cheered them in, and practically served them food, to later going through the offices everyone we ran into was loading them down with any pens, notebooks, food, etc. that they could find.  It seemed like a lot of positivity when Americans might be more conscious about "spoiling" them or giving them too much.  Also, many of my coworkers seemed to take less of a friend or authority role, and instead be more like a parent, and almost lecture the kids about the most basic stuff.  I heard the most typical things said to the kids like "Stay in school", "Don\'t play with matches", "Have faith in God even if your parents don\'t", etc., and me as an American felt like this would be seen as stepping on the parent\'s toes and doing their job in an overly forward way in the US.  Note that the religious comment obviously has a much different feel here in Peru where the vast majority of the people and the schools are Catholic (including this school), but it still seemed like they were preaching to these kids they just met like they won\'t get it anywhere else.
+Huacachina had a cool story behind it\'s founding and name.  Supposedly a boy callend Chino was wandering around in the dunes at night and saw a girl walking alone.  He followed her to a lagoon and saw her disappear into the water.  He came back the next night and waited for her.  When he saw her again he approached her, and she told him her name was Huaca.  I believe the story ended with him leading the rest of his people to this oasis and then disappearing into the lagoon with Huaca.  Anyway it was a cool place, amazing seeing an actual town like this in the middle of huge dunes like Dumont sand dunes back near home.
+</p>
+<img src = "http://www.justinmccandless.com/uploads/images/ica2.jpg">
+<img src = "http://www.justinmccandless.com/uploads/images/ica3.jpg">
+<p>
+I\'v done a lot of four wheeling around Las Vegas and at sand dunes like Dumont back home, so I pretty much assumed the touristy version of this in the dune buggy ride we had planned would be pretty lame in comparison.  The dune buggy was more powerful than I expected though and could actually climb some of the bigger dunes, and our driver was pretty fun, so it was actually a pretty good time.  The dune buggy also drove us to the top of some dunes and let us go sand boarding down.  The sand boards they had were like home made plywood + velcro things, but if you waxed them enough you could move pretty well.  Most people went down laying on the sand boards like a sled, but you could go down on your feet if you didn\'t treat it too much like a snowboard and just aimed pretty straight.
+</p>
+<img src = "http://www.justinmccandless.com/uploads/images/ica4.jpg">
+<img src = "http://www.justinmccandless.com/uploads/images/ica5.jpg">
+<img src = "http://www.justinmccandless.com/uploads/images/ica6.jpg">
+<p>
+I was trying it standing up for the most part, but we came to one dune that was huge and straight down, so I decided to sled that one since I couldn\'t carve like on a snowboard (justifying why I\'m not a wuss).  Anyway, I really wanted to get a ton of speed if I wasn\'t going to stand up, so I came flying down than hill without dragging my feet at all.  There was an upward slope after you reached the bottom to slow back down, and so people climbed up the hill to get out of the way of others going down.  I was going a bit too fast though, and there was a guy directly in my path.  He saw my coming from awhile back, so my thought was "Maximum speed never slow down he will move!".  He didn\'t though, he was kind of frozen in a decision to go left or right, and pretty much dove to the side at the last second.  I took a foot directly to the head, but luckily we were both got up right away and were pretty much fine.  I noticed a cut on my side a few minutes later was probably from his board (picture below), but it was fine after I cleaned it up with the hotel\'s first aid kit later.  The next day I could not keep anything in my stomach without vomiting, but I hope that was due to the food and not a concussion.  I was fine after that day anyway.
+</p>
+<img src = "http://www.justinmccandless.com/uploads/images/ica7.jpg">
+<p>
+We also got to go check out a vineyard, since there are a bunch in this area.  The most famous alocholic drink in Peru is pisco, which is made from grapes like wine.  If I\'m not mistaken it is completely clear though.  People make a margarita-like mixed drink called pisco sour from it, which has lime and egg whites and stuff in it.  I don\'t drink, but it was definitely cool to see the vineyard and all of their equipment.  I even saw some of the farm workers coming to work, which I thought was really interesting.  The place had been a monastery at some point and it reminded me of it so much while I was there.
+</p>
+<img src = "http://www.justinmccandless.com/uploads/images/ica8.jpg">
+<p>
+Nazca, where the Nazca lines are, is also in this area.  I was really excited about these, but you pretty much have to take a plane to see them, so I wasn\'t able to.  We did wind up seeing another similar petroglyph though nearby...
+</p>
+<img src = "http://www.justinmccandless.com/uploads/images/ica9.jpg">
+<p>
+This is on a hill facing the ocean outside of Paracas (~50km inland from Ica).  People actually don\'t know who made it, some say the indigenous culture nearby, others say sailors used it as a landmark after the Spanish came.  It is significantly deeper than the Nazca lines, and not in the same area as those.  It\'s supposed to be a cactus which is believed to have medicinal properties from the region.  You can see how barren the desert is here, there is not a plant to be seen.  Our guide said that the reason the picture hasn\'t been erased is because it probably hasn\'t rained here since it was made.
 </p>
 <p>
-This could have been affected by the fact that the kids were not like the neighbor\'s kids or something, they were from a very poor area outside of Lima.  I was told most parents worked in furniture factories and the like by my coworkers, but I\'m not personally familiar with the area.  Some of them had fairly beat up back packs or shoes, but it wasn\'t horribly obvious by hanging out with them.  My "little brother" in particular did have it bad though, he was missing about half of one of his fingers and his upper lip area was dented inward so that it was pretty hard to understand his speech.  I couldn\'t tell if it was a birth defect or it had been caused later in life.  He got by fine though, and he seemd to be accepted like normal by the other kids.
+We did go on a boat to see that cactus drawing, but the main attraction wasn\'t that, it was the Islas Ballestas.
+</p>
+<img src = "http://www.justinmccandless.com/uploads/images/ica10.jpg">
+<p>
+These are desert islands that are completely filled with birds!  Not exaggerating, in the following picture, all of the little black dots are birds.
+</p>
+<img src = "http://www.justinmccandless.com/uploads/images/ica11.jpg">
+<p>
+The animals there were pretty cool though, we saw a bunch of sea lions and even some penguins.
+</p>
+<img src = "http://www.justinmccandless.com/uploads/images/ica12.jpg">
+<img src = "http://www.justinmccandless.com/uploads/images/ica13.jpg">
+<p>
+And below are a few more of my favorite pictures from the islands.  The building and the guys in the boat are there to collect guano, which is sold as fertilizer.
+</p>
+<img src = "http://www.justinmccandless.com/uploads/images/ica14.jpg">
+<img src = "http://www.justinmccandless.com/uploads/images/ica15.jpg">
+<img src = "http://www.justinmccandless.com/uploads/images/ica16.jpg">
+<p>
+That was my trip to Ica.  We took the bus line 
+<a href = "http://www.cruzdelsur.com.pe/">Cruz del Sur</a>
+, which was actually really nice and definitely worth the cost.  Peru has a lot of potential cool stuff to see but not much of a tourism industry, so I keep an eye out for companies or attractions that get it right.
 </p>
 <p>
-These were just a few interesting things I noticed while volunteering and wanted to write about.  I had a ton of fun volunteering here, and I hope we gave the kids an awesome day.  For me, it was an incredible experience being able to see and participate in something like this, not just back in Las Vegas but in another country!
+Lastly, I\'m trying out using Picasa to share my photos from this blog.  So check out the rest of my ~200 pics from this trip below courtesy of Picasa, and we\'ll see if I should do this for the rest of my posts.
 </p>
-');
-
+<table style="width:194px;"><tr><td align="center" style="height:194px;background:url(https://picasaweb.google.com/s/c/transparent_album_background.gif) no-repeat left"><a href="https://picasaweb.google.com/101532221547292067981/Ica?authuser=0&feat=embedwebsite"><img src="https://lh3.googleusercontent.com/-MXsog1rJXD0/Tq93l3J5NQE/AAAAAAAAAwo/oq96Gogc_eA/s160-c/Ica.jpg" width="160" height="160" style="margin:1px 0 0 4px;"></a></td></tr><tr><td style="text-align:center;font-family:arial,sans-serif;font-size:11px"><a href="https://picasaweb.google.com/101532221547292067981/Ica?authuser=0&feat=embedwebsite" style="color:#4D4D4D;font-weight:bold;text-decoration:none;">Ica</a></td></tr></table>
+'
+);
 
 
 /*
@@ -34,16 +79,6 @@ As negative as this post may have sounded, things like this actually just remind
 
 --
 
-Last weekend I was able to travel again so I need to write about it!  This trip was with a bunch of our interns here to Ica in the desert south of Lima.  Nazca, where the Nazca lines are, is in this area.  I was really excited about these, but you pretty much have to take a plane to see them so I wasn't able to.  We did wind up seeing another similar petroglyph though nearby...
-
-This is on a hill facing the ocean outside of Paracas (~50km inland from Ica).  People actually don't know who made it, some say the indigenous culture nearby, others say sailors used it as a landmark after the Spanish came.  It is significantly deeper than the Nazca lines, and not in the same area as those.  It's supposed to be a cactus which is believed to have medicinal properties from the region.  You can see how barren the desert is here, there is not a plant to be seen.  Our guide said that the reason the picture hasn't been erased is because it probably hasn't rained here since it was made.
-
-We did go on a boat to see that cactus drawing, but the main attraction wasn't that, it was the Islas Ballestas. 
-
-*/
-
-/*
-Volunteering
 
 --- 
 
