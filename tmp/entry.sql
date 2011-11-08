22d4b3ccb63dcd59bf7131a6c7e8c0be49b60270
justin <justinjmc80@gmail.com>
2011-11-08 18:17:46 -0500

    Fixed tags in keywords, added video for Feliz Cumpleanos, added entry.sql for that and Peru tag

--- tmp/entry.sql
+++ tmp/entry.sql
@@ -1,65 +1,38 @@
-INSERT INTO Entries VALUES (58, 59, 'http://www.justinmccandless.com/index.php?entry=59', 'Ica', '2011-11-01 13:00:00', '', 'Travel', 
+INSERT INTO dat_entries VALUES (59, 'Feliz Cumpeaños', '2011-11-08 18:00:00', '',
 '
 <p>
-The other weekend I was able to travel again so I need to write about it!  This trip was with a bunch of our interns here to Ica in the desert south of Lima.  We stayed in a small town outside Ica called Huacachina, which is honestly like an oasis in the middle of sand dunes.  We got to see a vineyard, go out on the dunes, and take a boat to some islands full of birds called Las Islas Ballestas.
-</p>
-<img src = "http://www.justinmccandless.com/uploads/images/ica1.jpg">
-<p>
-Huacachina had a cool story behind it\'s founding and name.  Supposedly a boy callend Chino was wandering around in the dunes at night and saw a girl walking alone.  He followed her to a lagoon and saw her disappear into the water.  He came back the next night and waited for her.  When he saw her again he approached her, and she told him her name was Huaca.  I believe the story ended with him leading the rest of his people to this oasis and then disappearing into the lagoon with Huaca.  Anyway it was a cool place, amazing seeing an actual town like this in the middle of huge dunes like Dumont sand dunes back near home.
-</p>
-<img src = "http://www.justinmccandless.com/uploads/images/ica2.jpg">
-<img src = "http://www.justinmccandless.com/uploads/images/ica3.jpg">
-<p>
-I\'v done a lot of four wheeling around Las Vegas and at sand dunes like Dumont back home, so I pretty much assumed the touristy version of this in the dune buggy ride we had planned would be pretty lame in comparison.  The dune buggy was more powerful than I expected though and could actually climb some of the bigger dunes, and our driver was pretty fun, so it was actually a pretty good time.  The dune buggy also drove us to the top of some dunes and let us go sand boarding down.  The sand boards they had were like home made plywood + velcro things, but if you waxed them enough you could move pretty well.  Most people went down laying on the sand boards like a sled, but you could go down on your feet if you didn\'t treat it too much like a snowboard and just aimed pretty straight.
-</p>
-<img src = "http://www.justinmccandless.com/uploads/images/ica4.jpg">
-<img src = "http://www.justinmccandless.com/uploads/images/ica5.jpg">
-<img src = "http://www.justinmccandless.com/uploads/images/ica6.jpg">
-<p>
-I was trying it standing up for the most part, but we came to one dune that was huge and straight down, so I decided to sled that one since I couldn\'t carve like on a snowboard (justifying why I\'m not a wuss).  Anyway, I really wanted to get a ton of speed if I wasn\'t going to stand up, so I came flying down than hill without dragging my feet at all.  There was an upward slope after you reached the bottom to slow back down, and so people climbed up the hill to get out of the way of others going down.  I was going a bit too fast though, and there was a guy directly in my path.  He saw my coming from awhile back, so my thought was "Maximum speed never slow down he will move!".  He didn\'t though, he was kind of frozen in a decision to go left or right, and pretty much dove to the side at the last second.  I took a foot directly to the head, but luckily we were both got up right away and were pretty much fine.  I noticed a cut on my side a few minutes later was probably from his board (picture below), but it was fine after I cleaned it up with the hotel\'s first aid kit later.  The next day I could not keep anything in my stomach without vomiting, but I hope that was due to the food and not a concussion.  I was fine after that day anyway.
-</p>
-<img src = "http://www.justinmccandless.com/uploads/images/ica7.jpg">
-<p>
-We also got to go check out a vineyard, since there are a bunch in this area.  The most famous alocholic drink in Peru is pisco, which is made from grapes like wine.  If I\'m not mistaken it is completely clear though.  People make a margarita-like mixed drink called pisco sour from it, which has lime and egg whites and stuff in it.  I don\'t drink, but it was definitely cool to see the vineyard and all of their equipment.  I even saw some of the farm workers coming to work, which I thought was really interesting.  The place had been a monastery at some point and it reminded me of it so much while I was there.
-</p>
-<img src = "http://www.justinmccandless.com/uploads/images/ica8.jpg">
-<p>
-Nazca, where the Nazca lines are, is also in this area.  I was really excited about these, but you pretty much have to take a plane to see them, so I wasn\'t able to.  We did wind up seeing another similar petroglyph though nearby...
-</p>
-<img src = "http://www.justinmccandless.com/uploads/images/ica9.jpg">
-<p>
-This is on a hill facing the ocean outside of Paracas (~50km inland from Ica).  People actually don\'t know who made it, some say the indigenous culture nearby, others say sailors used it as a landmark after the Spanish came.  It is significantly deeper than the Nazca lines, and not in the same area as those.  It\'s supposed to be a cactus which is believed to have medicinal properties from the region.  You can see how barren the desert is here, there is not a plant to be seen.  Our guide said that the reason the picture hasn\'t been erased is because it probably hasn\'t rained here since it was made.
+I recently celebrated my birthday here in Peru, my first time celebrating it abroad.  Actually it wasn\'t quite as homesick as it sounds though fortunately.  I definitely missed my family and everyone back home, but a bunch of my friends here made it really fun.
 </p>
 <p>
-We did go on a boat to see that cactus drawing, but the main attraction wasn\'t that, it was the Islas Ballestas.
-</p>
-<img src = "http://www.justinmccandless.com/uploads/images/ica10.jpg">
-<p>
-These are desert islands that are completely filled with birds!  Not exaggerating, in the following picture, all of the little black dots are birds.
-</p>
-<img src = "http://www.justinmccandless.com/uploads/images/ica11.jpg">
-<p>
-The animals there were pretty cool though, we saw a bunch of sea lions and even some penguins.
-</p>
-<img src = "http://www.justinmccandless.com/uploads/images/ica12.jpg">
-<img src = "http://www.justinmccandless.com/uploads/images/ica13.jpg">
-<p>
-And below are a few more of my favorite pictures from the islands.  The building and the guys in the boat are there to collect guano, which is sold as fertilizer.
-</p>
-<img src = "http://www.justinmccandless.com/uploads/images/ica14.jpg">
-<img src = "http://www.justinmccandless.com/uploads/images/ica15.jpg">
-<img src = "http://www.justinmccandless.com/uploads/images/ica16.jpg">
-<p>
-That was my trip to Ica.  We took the bus line 
-<a href = "http://www.cruzdelsur.com.pe/">Cruz del Sur</a>
-, which was actually really nice and definitely worth the cost.  Peru has a lot of potential cool stuff to see but not much of a tourism industry, so I keep an eye out for companies or attractions that get it right.
+At work I definitely felt appreciated.  As is custom in our office, everyone I work with came up to me and personally told me happy birthday and shook my hand at some point throughout the day.  According to another work tradition, they all took me out to eat during lunch at a nice restaurant.  We went to a rotisserie chicken (pollo a la brasa in Spanish) place in the mall nearby.  This is really common food here, I should have taken a picture.  It\'s basically a quarter of a chicken, plus fries and a couple different Peruvian style sauces.  Ají is the most famous, a spicy yellow, green, or red sauce usually made from fresh peppers.  Also there is some sort of green sauce with a more garlic type flavor, and a white cool tasting sauce that is really good.  They made the restaurant people sing me happy birthday and give me a desert with a candle too, even though that part might just embarrass me...  Worth it for the desert though.
 </p>
 <p>
-Lastly, I\'m trying out using Picasa to share my photos from this blog.  So check out the rest of my ~200 pics from this trip below courtesy of Picasa, and we\'ll see if I should do this for the rest of my posts.
+Oh and back at the office, a coworker insisted that I call my parents right that instant so that they didn\'t worry about me on my birthday.  He lent me his iPhone so I could log in to Skype, and I took a break to give them all a call.
 </p>
-<table style="width:194px;"><tr><td align="center" style="height:194px;background:url(https://picasaweb.google.com/s/c/transparent_album_background.gif) no-repeat left"><a href="https://picasaweb.google.com/101532221547292067981/Ica?authuser=0&feat=embedwebsite"><img src="https://lh3.googleusercontent.com/-MXsog1rJXD0/Tq93l3J5NQE/AAAAAAAAAwo/oq96Gogc_eA/s160-c/Ica.jpg" width="160" height="160" style="margin:1px 0 0 4px;"></a></td></tr><tr><td style="text-align:center;font-family:arial,sans-serif;font-size:11px"><a href="https://picasaweb.google.com/101532221547292067981/Ica?authuser=0&feat=embedwebsite" style="color:#4D4D4D;font-weight:bold;text-decoration:none;">Ica</a></td></tr></table>
+<p>
+Then later that night I got to go out to eat again with the other interns and AIESECers!  A ton of people wound up coming, much more than I expected, and I was very grateful.  They took me to Chili\'s, which normally I would be against because I want to eat all the Peruvian food I can here and not stuff I can get back in the US, but actually Chili\'s was exactly what I needed.  Once I saw the menu I realized how much I had missed American food.  The menu was almost the same as the US menu (with a little bit more white rice of course).  I ordered ribs, which I love in the US, and it tasted almost the same and I loved it.  It also came with yellow corn like we have in the US, which was amazing after eating so much of the giant white corn they have here.  They even gave me a bunch of presents that I wasn\'t expecting at all.  And of course they had to sing me happy birthday again at the end (one more desert).
+</p>
+<p>
+By the way, I have to explain the happy birthday song in Peru.  It starts out like the American version in English but with a Peruvian accent, a slightly different tune, and no "dear [birthday person]" part.  Then it repeats again in Spanish while clapping to the beat, and I believe the words are "Cumpleaños feliz, deseamos a ti."  It took me a few birthdays at the office to figure it out.
+</p>
+<p>
+Lastly, AIESEC Pacifico here (the kids at the University of the Pacific taking care of me), gave me an awesome gift that they made for us trainees.  It\'s a video wishing me happy birthday, which I embedded below.  Really cool gift, and they gave it to me in a DVD case and everything that they made.  Overall it was an awesome birthday, especially considering my last 4 birthdays were spent 90% studying in the library at Michigan.  And I still have at least one more present to open when I get back home in December, so I get to drag it out as well!
+</p>
+<br><br>
+<video width="320" height="240" controls="controls">
+  <source src="http://www.justinmccandless.com/uploads/videos/felizcumpleanos.ogg" type="video/ogg" />
+  Your browser does not support the video tag in Ogg format.  Download the video instead here:
+  <a href = "http://www.justinmccandless.com/uploads/videos/felizcumpleanos.ogg">felizcumpleanos.ogg</a>
+</video>
+<br><br> 
 '
 );
+
+
+INSERT INTO rel_entries_tags VALUES (59, 59, 5), (60, 59, 3);
+
+
+INSERT INTO dat_tags VALUES (5, 'Peru', "Right out of college I took a six month internship working as a software engineer for Telefonica in Lima, Peru, for the second half of 2011.  This tag contains anything about my travels, opinions, and anything else during my time in Peru.");
 
 
 /*
